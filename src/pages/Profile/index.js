1ae601b5085f8917b39d71b406b147401e0c6969
João Cleber Colchalski <joaocleber.jc10@gmail.com>
2024-10-09 13:13:08 -0300

    Atualizando Nome

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,37 +1,91 @@
 import React, { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/auth";
+import Feather from 'react-native-vector-icons/Feather'
 
+
+import { Modal, Platform } from "react-native";
 import {
     Container,
+    UploadButton,
+    UploadText,
+    Avatar,
     Name,
     Email,
     Button,
-    ButtonText
+    ButtonText,
+    ModalContainer,
+    ButtonBack,
+    Input
 } from "./styles";
 import Header from "../../components/Header";
 
 export default function Profile() {
     const { user, signOut } = useContext(AuthContext)
 
+    const [name, setName] = useState(user?.name)
+    const [url, setUrl] = useState(null)
+    const [visibleModal, setVisibleModal] = useState(false)
+
     async function handleSignOut() {
         await signOut()
+    }
+
+    async function updateProfile() {
+        alert('teste')
     }
 
     return (
         <Container>
             <Header />
 
-            <Name>{user.name}</Name>
+            {url ? (
+                <UploadButton>
+                    <UploadText>+</UploadText>
 
-            <Email>{user.email}</Email>
+                    <Avatar
+                        source={{ uri: url }}
+                    />
+                </UploadButton>
+            ) : (
+                <UploadButton>
+                    <UploadText>+</UploadText>
 
-            <Button>
-                <ButtonText>Atualizar Perfil</ButtonText>
+                    <Avatar
+                        source={require('../../assets/avatar.png')}
+                    />
+                </UploadButton>
+            )}
+
+            <Name numberOfLines={1}>{user?.name}</Name>
+
+            <Email>{user?.email}</Email>
+
+            <Button bg='#428cfd' onPress={() => setVisibleModal(true)}>
+                <ButtonText color='#fff'>Atualizar Perfil</ButtonText>
             </Button>
 
-            <Button onPress={() => signOut()}>
-                <ButtonText>Sair</ButtonText>
+            <Button bg='#ddd' onPress={handleSignOut} >
+                <ButtonText color='#3b3b3b'>Sair</ButtonText>
             </Button>
+
+            <Modal visible={visibleModal} animationType='slide' transparent={true}>
+                <ModalContainer behavior={Platform.OS === 'android' ? '' : 'padding'}>
+                    <ButtonBack onPress={() => setVisibleModal(false)}>
+                        <Feather name='arrow-left' size={22} color='#121212' />
+                        <ButtonText color='#121212'>Voltar</ButtonText>
+                    </ButtonBack>
+
+                    <Input
+                        placeholder={user?.name}
+                        value={name}
+                        onChangeText={(text) => setName(text)}
+                    />
+
+                    <Button bg='#428cfd' onPress={updateProfile}>
+                        <ButtonText color='#fff'>Salvar</ButtonText>
+                    </Button>
+                </ModalContainer>
+            </Modal>
         </Container>
     )
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -2,21 +2,98 @@ import styled from "styled-components/native"
 
 export const Container = styled.SafeAreaView`
     flex: 1;
+    align-items: center;
     background-color: #353840;
 `
 
-export const Name = styled.Text`
+export const UploadButton = styled.TouchableOpacity`
+    margin-top: 20%;
+    background-color: #fff;
+    width: 165px;
+    height: 165px;
+    border-radius: 90px;
+    justify-content: center;
+    align-items: center;
+    z-index: 8;
+`
 
+export const UploadText = styled.Text`
+    font-size: 55px;
+    position: absolute;
+    color: #e52246;
+    opacity: 0.5;
+    z-index: 99;
+`
+
+export const Avatar = styled.Image`
+    width: 160px;
+    height: 160px;
+    border-radius: 80px;
+`
+
+export const Name = styled.Text`
+    margin-top: 20px;
+    margin-right: 20px;
+    margin-left: 20px;
+    font-size: 28px;
+    font-weight: bold;
+    color: #fff;
 `
 
 export const Email = styled.Text`
-
+    color: #fff;
+    margin-top: 10px;
+    margin-right: 20px;
+    margin-left: 20px;
+    font-size: 18px;
+    font-style: italic;
 `
 
 export const Button = styled.TouchableOpacity`
-
+    margin-top: 16px;
+    background-color: ${props => props.bg};
+    width: 80%;
+    height: 50px;
+    border-radius: 4px;
+    align-items: center;
+    justify-content: center;
 `
 
 export const ButtonText = styled.Text`
+    font-size: 18px;
+    color: ${props => props.color};
+    font-weight: bold;
+    font-style: italic;
+`
 
+export const Modal = styled.Modal`
+    align-self: flex-end;
+`
+
+export const ModalContainer = styled.KeyboardAvoidingView`
+    background-color: #fff;
+    width: 100%;
+    height: 70%;
+    position: absolute;
+    bottom: 0;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ButtonBack = styled.TouchableOpacity`
+    flex-direction: row;
+    position: absolute;
+    left: 15px;
+    top: 15px;
+    align-items: center;
+`
+
+export const Input = styled.TextInput`
+    background-color: #ddd;
+    width: 90%;
+    border-radius: 4px;
+    padding: 10px;
+    font-size: 17px;
+    color: #121212;
+    text-align: center;
 `
